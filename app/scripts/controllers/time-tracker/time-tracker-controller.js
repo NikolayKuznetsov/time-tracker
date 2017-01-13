41460b16f81dae71dc74e962ba65b500eee42bbb
Nikolay Kuznesov <nikolay.kuznesov@mainsoft.by>
2017-01-13 15:58:13 +0300

    commit

--- app/scripts/controllers/time-tracker/time-tracker-controller.js
+++ app/scripts/controllers/time-tracker/time-tracker-controller.js
@@ -6,32 +6,63 @@ var TimeTrackerCtrl = function (LocalStorageService) {
         '1': {
             'title': 'test 1',
             'time': '1',
+            'message': 'lorem 1',
             'cost': '1200'
         },
         '2': {
             'title': 'test 2',
             'time': '2',
+            'message': 'lorem 2',
             'cost': '100'
+        },
+        '3': {
+            'title': 'test 3',
+            'time': '3',
+            'message': 'lorem 3',
+            'cost': '300'
         }
     };
     tt.title = 'Time tracker';
     tt.values = [];
+    tt.addTask = {};
+    tt.addTask.name = '';
+    tt.addTask.time = '';
+    tt.addTask.message = '';
+    tt.addTask.cost = '';
 
     tt.initTimeTrackerCtrl = function () {
-        console.log("init");
-        tt.generationValue();
+        // tt.generationValue();
         tt.getValue();
     };
 
     tt.generationValue = function () {
-        console.log("generationValue");
         LocalStorageService.setObject(tt.keyLocalTimeTracker, tt.jsonLocalTimeTracker);
+        tt.getValue();
     };
 
     tt.getValue = function () {
-        console.log("getValue");
         tt.values = LocalStorageService.getObject(tt.keyLocalTimeTracker);
-        console.log(tt.values)
+    };
+
+    tt.removeValue = function () {
+        LocalStorageService.removeString(tt.keyLocalTimeTracker);
+        tt.getValue();
+    };
+
+    tt.addNewTask = function () {
+        // console.log(tt.addTask.name);
+        // console.log(tt.addTask.time);
+        // console.log(tt.addTask.message);
+        // console.log(tt.addTask.cost);
+        // tt.values[tt.values.length];
+        console.log(tt.values);
+        // tt.values.push({
+        //     title: tt.addTask.name,
+        //     time: tt.addTask.time,
+        //     message: tt.addTask.message,
+        //     cost: tt.addTask.cost
+        // });
+        // console.log(tt.values);
     };
 
     tt.initTimeTrackerCtrl();
