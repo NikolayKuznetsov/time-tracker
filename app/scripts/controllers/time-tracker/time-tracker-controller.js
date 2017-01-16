5cea294507fc1fab7991538ef40b3fe3dcb121de
Nikolay Kuznesov <nikolay.kuznesov@mainsoft.by>
2017-01-16 12:20:40 +0300

    Add Time format

--- app/scripts/controllers/time-tracker/time-tracker-controller.js
+++ app/scripts/controllers/time-tracker/time-tracker-controller.js
@@ -144,6 +144,22 @@ var TimeTrackerCtrl = function (LocalStorageService, $timeout) {
     };
 
     /*
+     * Time format
+     * */
+    tt.timeFormat = function (number) {
+        function num(val) {
+            val = Math.floor(val);
+            return val < 10 ? '0' + val : val;
+        }
+
+        var sec = number,
+            hours = sec / 3600 % 24,
+            minutes = sec / 60 % 60,
+            seconds = sec % 60;
+        return num(hours) + ":" + num(minutes) + ":" + num(seconds);
+    };
+
+    /*
      * Init Time Tracker Controller
      * */
     tt.initTimeTrackerCtrl();
