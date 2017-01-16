93d59326cd72efa0b453478f4ffe6959d25349a2
Nikolay Kuznesov <nikolay.kuznesov@mainsoft.by>
2017-01-16 10:10:19 +0300

    Edit json and logic

--- app/scripts/controllers/time-tracker/time-tracker-controller.js
+++ app/scripts/controllers/time-tracker/time-tracker-controller.js
@@ -1,27 +1,25 @@
 var TimeTrackerCtrl = function (LocalStorageService) {
     var tt = this;
 
-    tt.keyLocalTimeTracker = 'time-tracker-values';
-    tt.jsonLocalTimeTracker = {
-        '1': {
+    tt.KEY_LOCAL_TIME_TRACKER = 'time-tracker-values';
+    tt.JSON_LOCAL_TIME_TRACKER = [
+        {
             'title': 'test 1',
             'time': '1',
             'message': 'lorem 1',
             'cost': '1200'
-        },
-        '2': {
+        }, {
             'title': 'test 2',
             'time': '2',
             'message': 'lorem 2',
             'cost': '100'
-        },
-        '3': {
+        }, {
             'title': 'test 3',
             'time': '3',
             'message': 'lorem 3',
             'cost': '300'
         }
-    };
+    ];
     tt.title = 'Time tracker';
     tt.values = [];
     tt.addTask = {};
@@ -30,41 +28,68 @@ var TimeTrackerCtrl = function (LocalStorageService) {
     tt.addTask.message = '';
     tt.addTask.cost = '';
 
+    /*
+     * Function init controller
+     * */
     tt.initTimeTrackerCtrl = function () {
-        // tt.generationValue();
         tt.getValue();
     };
 
+    /*
+     *  Generation tasks
+     * */
     tt.generationValue = function () {
-        LocalStorageService.setObject(tt.keyLocalTimeTracker, tt.jsonLocalTimeTracker);
+        tt.addValueLocalStorage(tt.JSON_LOCAL_TIME_TRACKER);
         tt.getValue();
     };
 
+    /*
+     *  Get all tasks with LocalStorage
+     * */
     tt.getValue = function () {
-        tt.values = LocalStorageService.getObject(tt.keyLocalTimeTracker);
+        tt.values = LocalStorageService.getObject(tt.KEY_LOCAL_TIME_TRACKER);
     };
 
+    /*
+     *  Write tasks for LocalStorage
+     * */
+    tt.addValueLocalStorage = function (jsonValues) {
+        LocalStorageService.setObject(tt.KEY_LOCAL_TIME_TRACKER, jsonValues);
+    };
+
+    /*
+     *  Remove all tasks with LocalStorage
+     * */
     tt.removeValue = function () {
-        LocalStorageService.removeString(tt.keyLocalTimeTracker);
+        LocalStorageService.removeString(tt.KEY_LOCAL_TIME_TRACKER);
         tt.getValue();
     };
 
+    /*
+     *  Add new task for list and LocalStorage
+     * */
     tt.addNewTask = function () {
-        // console.log(tt.addTask.name);
-        // console.log(tt.addTask.time);
-        // console.log(tt.addTask.message);
-        // console.log(tt.addTask.cost);
-        // tt.values[tt.values.length];
-        console.log(tt.values);
-        // tt.values.push({
-        //     title: tt.addTask.name,
-        //     time: tt.addTask.time,
-        //     message: tt.addTask.message,
-        //     cost: tt.addTask.cost
-        // });
-        // console.log(tt.values);
+        tt.values.push({
+            title: tt.addTask.name,
+            time: tt.addTask.time,
+            message: tt.addTask.message,
+            cost: tt.addTask.cost
+        });
+        tt.addValueLocalStorage(tt.values);
+        // Clean form
+        tt.cleanForm();
     };
 
+    /*
+     * Clean from create task
+     * */
+    tt.cleanForm = function () {
+        tt.addTask.name = tt.addTask.time = tt.addTask.message = tt.addTask.cost = '';
+    };
+
+    /*
+     * Init Time Tracker Controller
+     * */
     tt.initTimeTrackerCtrl();
 
 };
