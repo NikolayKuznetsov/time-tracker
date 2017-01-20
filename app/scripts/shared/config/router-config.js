5a64e88ef245b9d4a8ede2908ab06badf89f13b1
Nikolay Kuznesov <nikolay.kuznesov@mainsoft.by>
2017-01-20 09:31:39 +0300

    ui router

--- app/scripts/shared/config/router-config.js
+++ app/scripts/shared/config/router-config.js
@@ -1,17 +1,21 @@
-var uiRouter = function ($stateProvider, $urlRouterProvider, $locationProvider) {
-    $locationProvider.hashPrefix('');
+angular.module('app').config(function ($stateProvider, $urlRouterProvider, $locationProvider) {
+
+    // $locationProvider.hashPrefix('');
+    $urlRouterProvider.otherwise('/');
+
     $stateProvider
         .state('index', {
             url: '/',
-            templateUrl: "index.html"
+            templateUrl: 'scripts/controllers/main/main-index.html'
         })
-        .state('timeTracker.report', {
-            url: "/report",
-            templateUrl: "scripts/controllers/time-tracker/report/report-time-tracker-index.html"
+        .state('timeTracker', {
+            url: '/time-tracker',
+            templateUrl: 'scripts/controllers/time-tracker/time-tracker-index.html'
+        })
+        .state('timeTrackerReport', {
+            url: '/time-tracker-report',
+            templateUrl: 'scripts/controllers/time-tracker/report/report-time-tracker-index.html'
         });
 
-    $urlRouterProvider.otherwise('/');
-};
-
-uiRouter.$inject = ['$stateProvider', '$urlRouterProvider', '$locationProvider'];
-angular.module('app').config(uiRouter);
+    $locationProvider.html5Mode(true);
+});
