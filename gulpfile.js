f21622e1b9370fb73c3411aa83c65bbaf82a8408
Nikolay Kuznesov <nikolay.kuznesov@mainsoft.by>
2017-01-21 12:01:44 +0300

    gulp build

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,21 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var prefix = require('gulp-autoprefixer');
-
 var useref = require('gulp-useref');
 var csso = require('gulp-csso');
 var gulpif = require('gulp-if');
 var uglify = require('gulp-uglify');
 var cleanDest = require('gulp-clean-dest');
-
 var webserver = require('gulp-webserver');
+
+var del = require('del');
+
+var config = {
+    path: {
+        build: 'build',
+        buildFont: 'build/fonts'
+    }
+};
 
 //SERVER
 gulp.task('server-run', function () {
@@ -37,15 +44,28 @@ gulp.task('styles', function () {
         .pipe(gulp.dest('./app/css'));
 });
 
+//CLEAN public
+gulp.task('clean', function () {
+    return del([config.path.build]);
+});
+
+// BUILD fonts
+gulp.task('build-fonts', function () {
+    return gulp.src('./app/libs/bootstrap/fonts/*.*')
+        .pipe(gulp.dest(config.path.buildFont));
+});
+
+//BUILD js and css
+gulp.task('build:app', function () {
+    return gulp.src('./app/*.html')
+        .pipe(useref())
+        .pipe(gulpif('*.css', csso()))
+        .pipe(gulp.dest(config.path.build));
+});
 
 //BUILD
-gulp.task('build', function () {
-    return gulp.src('./app/*.html')
-        .pipe(cleanDest('public'))
-        .pipe(useref())
-        .pipe(gulpif('*.js', uglify()))
-        .pipe(gulpif('*.css', csso()))
-        .pipe(gulp.dest('public'));
+gulp.task('build', ['clean'], function () {
+    gulp.start('build-fonts', 'build:app');
 });
 
 
