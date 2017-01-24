14f011eab5633784629785d00b5049338bcdf45a
Nikolay Kuznesov <nikolay.kuznesov@mainsoft.by>
2017-01-24 11:48:25 +0300

    Add page login and ui-view

---
+++ app/scripts/controllers/header/header-controller.js
@@ -0,0 +1,8 @@
+var HeaderCtrl = function () {
+    var vm = this;
+
+
+};
+
+HeaderCtrl.$inject = [];
+angular.module('app').controller('HeaderCtrl', HeaderCtrl);
--- app/scripts/shared/config/router-config.js
+++ app/scripts/shared/config/router-config.js
@@ -1,20 +1,49 @@
 angular.module('app').config(function ($stateProvider, $urlRouterProvider, $locationProvider) {
 
+    var header = {
+        templateUrl: 'scripts/controllers/header/header-index.html',
+        controller: 'HeaderCtrl',
+        controllerAs: 'header'
+    };
+
     // $locationProvider.hashPrefix('');
     $urlRouterProvider.otherwise('/');
 
     $stateProvider
         .state('index', {
             url: '/',
-            templateUrl: 'scripts/controllers/main/main-index.html'
+            views: {
+                'main': {
+                    templateUrl: 'scripts/controllers/main/main-index.html'
+                },
+                'header': header
+            }
+        })
+        .state('login', {
+            url: '/login',
+            views: {
+                'main': {
+                    templateUrl: 'scripts/controllers/login/login-index.html'
+                }
+            }
         })
         .state('timeTracker', {
             url: '/time-tracker',
-            templateUrl: 'scripts/controllers/time-tracker/time-tracker-index.html'
+            views: {
+                'main': {
+                    templateUrl: 'scripts/controllers/time-tracker/time-tracker-index.html'
+                },
+                'header': header
+            }
         })
         .state('timeTrackerReport', {
             url: '/time-tracker-report',
-            templateUrl: 'scripts/controllers/time-tracker/report/report-time-tracker-index.html'
+            views: {
+                'main': {
+                    templateUrl: 'scripts/controllers/time-tracker/report/report-time-tracker-index.html'
+                },
+                'header': header
+            }
         });
         // .state('tablePagination', {
         //     url: '/table-pagination',
